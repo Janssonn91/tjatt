deaae1be2368b72e6377b5d59ca4725545cbd74a
Niklas J <Niklas.Jansson91@hotmail.com>
2018-10-17 09:51:08 +0200

    Tagit bort onödiga filer och mappar.

--- book-importer/book.class.js
+++ book-importer/book.class.js
@@ -1,22 +1,22 @@
-const Schema = require('mongoose').Schema;
-const ModelAndRoutes = require('./model-and-routes.class');
+// const Schema = require('mongoose').Schema;
+// const ModelAndRoutes = require('./model-and-routes.class');
 
-module.exports = class Book extends ModelAndRoutes {
+// module.exports = class Book extends ModelAndRoutes {
 
-  static get schema(){
-    return {
-      author: {
-        type: Schema.Types.ObjectId,
-        ref: 'Author'
-      },
-      country: String,
-      imageLink: String,
-      language: String,
-      link: String,
-      pages: Number,
-      title: String,
-      year: Number
-    }
-  }
+//   static get schema(){
+//     return {
+//       author: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'Author'
+//       },
+//       country: String,
+//       imageLink: String,
+//       language: String,
+//       link: String,
+//       pages: Number,
+//       title: String,
+//       year: Number
+//     }
+//   }
 
-}
+// }
