441a66be1722ee4a8384dc9f3967dd9ebb1f980f
PaulinaHT <paulina.hertor@gmail.com>
2018-10-18 22:11:32 +0200

    Created a Server.js for testing

---
+++ src/components/Server/Server.js
@@ -0,0 +1,34 @@
+import './Server.scss';
+@observer export default class Server extends Component{
+
+@observable urlToSet = '';
+
+
+  start(){
+    this.createStoreConnectedProperties({
+      repo: []
+    });
+  }
+
+  editText(e){
+    this.urlToSet = e.currentTarget.value;
+  }
+  checkForEnter(e){
+    if(e.key ==='Enter'){
+    }
+  } 
+
+  submit = () => {
+    console.log(this.urlToSet)
+    fetch('/api/test', {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify({url: this.urlToSet}), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+
+  
+}
