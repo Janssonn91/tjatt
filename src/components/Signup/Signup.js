fa19747ee670e1c71257de9dfd15e3bc5e0c6c79
Johan Anderberg <johan_anderberg79@hotmail.com>
2018-10-18 21:53:15 +0200

    Preparing for saving userinfo

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,7 +1,44 @@
 import './Signup.scss';
 
 export default class Signup extends Component {
+
+  usernameToSet;
+  passWordToSet;
+  nicknameToSet;
+  person;
+
   async start() {
     
+  }
+
+  usernameChange(e){
+    this.usernameToSet = e.currentTarget.value;
+    //console.log(this.usernameToSet);
+  }
+
+  passwordChange(e){
+    this.passWordToSet = e.currentTarget.value;
+    //console.log(this.passWordToSet);
+  }
+
+  nicknameChange(e){
+    this.nicknameToSet = e.currentTarget.value;
+  }
+
+  async createUser() {
+    this.person = {
+      name : this.usernameToSet,
+      password : this.passWordToSet,
+      nickname : this.nicknameToSet
+    };
+    console.log(this.person);
+    let test = new User();
+    console.log(test);
+    await test.save({
+      name: this.usernameToSet,
+      password: this.passWordToSet,
+      nickname: this.nicknameToSet
+    });
+    console.log('hej din get');
   }
 }
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -5,17 +5,21 @@
       <Col sm="12" md={{ size: 6, offset: 3 }} lg={{ size: 4, offset: 4 }}>
         <FormGroup className="mt-4">
           <Label for="username">Username</Label>
-          <Input type="email" name="email" id="username" placeholder="with a placeholder" />
+          <Input type="text" name="text" id="username" placeholder="with a placeholder" value={this.userNameToSet} onChange= {e => this.usernameChange(e)} />
+        </FormGroup>
+        <FormGroup className="mt-4">
+          <Label for="nickname">Nickname</Label>
+          <Input type="text" name="text" id="nickname" placeholder="with a placeholder" value={this.nicknameToSet} onChange= {e => this.nicknameChange(e)} />
         </FormGroup>
         <FormGroup>
           <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" value={this.passWordToSet} onChange= {e => this.passwordChange(e)} />
         </FormGroup>
       </Col>
     </Row>
     <Row>
       <Col sm="12" md="12" lg="12" className="mt-4 mb-5 text-center">
-        <Button color="info">Sign up</Button>
+        <Button color="info" onClick= {e => this.createUser(e)}>Sign up</Button>
       </Col>
     </Row>
 
