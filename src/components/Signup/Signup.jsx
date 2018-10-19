faab0a22453845c8b73f86c0d0782a3fa5bd7a10
Emil <emil_emil94@hotmail.com>
2018-10-19 13:22:52 +0200

    Confirm password

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -5,11 +5,15 @@
       <Col sm="12" md={{ size: 6, offset: 3 }} lg={{ size: 4, offset: 4 }}>
         <FormGroup className="mt-4">
           <Label for="username">Username</Label>
-          <Input type="email" name="email" id="username" placeholder="with a placeholder" />
+          <Input type="email" name="email" id="username" placeholder="Choose username" />
         </FormGroup>
         <FormGroup>
           <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          <Input type="password" name="password" id="examplePassword" placeholder="Choose password" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="confirmPassword">Confirm Password</Label>
+          <Input type="password" name="password" id="confirmPassword" placeholder="Confirm password" />
         </FormGroup>
       </Col>
     </Row>
