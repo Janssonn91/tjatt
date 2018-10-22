d9b76a45327bc05f2036373a3a21d7602f956079
PaulinaHT <paulina.hertor@gmail.com>
2018-10-22 15:59:13 +0200

    Add spawned npm processes

--- express/routes/api.js
+++ express/routes/api.js
@@ -5,9 +5,10 @@ const promisifiedExec = util.promisify(exec); //Gör så att exec await kan anv�
 
 router.post('/test', async (req, res) => {
   console.log(req.body)
-  await promisifiedExec(`cd repos && git clone ${req.body.url} ${req.body.projectName} && cd ${req.body.projectName} && npm i` );
-  
-
+  await promisifiedExec(`cd repos && git clone ${req.body.url} ${req.body.projectName} && cd ${req.body.projectName} && npm install` );
+  let process = spawn('npm.cmd', ['start'], {cwd: `./repos/${req.body.projectName}`})
+  process.on('error', error => console.log('error: ' + error))
+  process.stdout.on('data', (data) => console.log('data: ' + data))
  res.json(req.body)
 })
 
