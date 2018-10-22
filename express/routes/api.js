2f66a238cef0aae6dcb3a69b96d784454dddddd1
PaulinaHT <paulina.hertor@gmail.com>
2018-10-22 15:04:49 +0200

    Add repos folder

--- express/routes/api.js
+++ express/routes/api.js
@@ -3,7 +3,7 @@ const {exec, spawn} = require('child_process');
 
 router.post('/test', (req, res) => {
   console.log(req.body)
-  exec(`git clone ${req.body.url} ${req.body.projectName} && cd ${req.body.projectName} && npm i` );
+  exec(`cd repos && git clone ${req.body.url} ${req.body.projectName} && cd ${req.body.projectName} && npm i` );
  res.json(req.body)
 })
 
