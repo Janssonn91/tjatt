9484d77e607f17626d68068912b58b9a8f5f24dd
PaulinaHT <paulina.hertor@gmail.com>
2018-10-22 15:27:55 +0200

    Promisify exec

--- express/routes/api.js
+++ express/routes/api.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const {exec, spawn} = require('child_process');
+const util = require('util');
+const promisifiedExec = util.promisify(exec); //Gör så att exec await kan användas
 
-router.post('/test', (req, res) => {
+router.post('/test', async (req, res) => {
   console.log(req.body)
-  exec(`cd repos && git clone ${req.body.url} ${req.body.projectName} && cd ${req.body.projectName} && npm i` );
+  await promisifiedExec(`cd repos && git clone ${req.body.url} ${req.body.projectName} && cd ${req.body.projectName} && npm i` );
+  
+
  res.json(req.body)
 })
 
