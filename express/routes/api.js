4e92d5562795f8ee022bcdc7b6e1539282a64712
Unknown <petter.f.andersson@gmail.com>
2018-10-24 11:44:22 +0200

    Randomize port number and overwrite in project's app.js before launching

--- express/routes/api.js
+++ express/routes/api.js
@@ -7,13 +7,13 @@ const path = require('path');
 const buf = require('buffer').Buffer;
 
 router.post('/addRepo', async (req, res) => {
-  console.log(req.body);
   await promisifiedExec(
     `cd repos && git clone ${req.body.url} ${req.body.projectName} && cd ${
       req.body.projectName
     } && npm install`
   );
-  let process = spawn('npm', ['start'], {
+  // Change back params to 'npm and ['start']
+  let process = spawn('node', ['app'], {
     cwd: `./repos/${req.body.projectName}`
   });
   process.on('error', error => console.log('error: ' + error));
@@ -21,19 +21,35 @@ router.post('/addRepo', async (req, res) => {
   res.json(req.body);
 
   /**
+   * Randomize a number between 1025-65535
+   * to use as port number
+   */
+  function randomizePortNumber() {
+    let newPortNumber = 1025 + Math.floor(Math.random() * 65535);
+    // Protect our specific ports for Mongo etc
+    let protectedPorts = [80, 443, 3000, 4500, 8080, 27017, 39812];
+    for (let i = 0; i < protectedPorts.length; i++) {
+      if (newPortNumber === protectedPorts[i]) {
+        randomizePortNumber();
+      }
+    }
+    return newPortNumber;
+  }
+
+  let port = randomizePortNumber();
+
+  /**
    * On POST request:
    * use path.resolve to get correct working directory
-   * test writing a file to make sure the location is correct
-   * and read app.js (after converting it from Buffer to string)
+   * read app.js to find current port number
+   * Change port number to one chosen by us then
+   * overwrite app.js file
+   * ** SPAGHETTI CODE WARNING **
    */
   await promisifiedExec(
     `cd ${path.resolve(`./repos/${req.body.projectName}`)}`,
     {},
     () => {
-      console.log(
-        'current directory: ' +
-          path.resolve(`./repos/${req.body.projectName}\n`)
-      );
       const filePath = path.resolve(`./repos/${req.body.projectName}`);
       fs.readFile(filePath + '/app.js', (err, data) => {
         if (err) throw err;
@@ -42,7 +58,18 @@ router.post('/addRepo', async (req, res) => {
           let stringIndex = appString.indexOf('app.listen'); //find "app.listen"
           let portIndex = stringIndex + 11; // index of portnumber ("3000")
           let portNumber = appString.slice(portIndex, portIndex + 4);
-          let newString = appString.replace(portNumber.toString(), '3500');
+          let newString = appString.replace(portNumber.toString(), port);
+
+          let stringIndexInConsoleLog = appString.indexOf('listening on port');
+          let portIndexInConsoleLog = stringIndexInConsoleLog + 18;
+          let portNumberInConsoleLog = appString.slice(
+            portIndexInConsoleLog,
+            portIndexInConsoleLog + 4
+          );
+          newString = newString.replace(
+            portNumberInConsoleLog.toString(),
+            port
+          );
           fs.writeFile(filePath + '/app.js', newString, err => {
             if (err) throw err;
           });
