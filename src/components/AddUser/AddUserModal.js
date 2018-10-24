23d45064a4fdf4ce43eb7c5cef0ef86945527af6
Nana Mochihara <nana.mochihara@gmail.com>
2018-10-24 12:16:54 +0200

    Filter users who don't exist in login user's contact

--- src/components/AddUser/AddUserModal.js
+++ src/components/AddUser/AddUserModal.js
@@ -1,6 +1,21 @@
 import './AddUserModal.scss';
 
-@withRouter @observer export default class AddUser extends Component {
-  start(){
+@observer export default class AddUser extends Component {
+
+  @observable users = [];
+  @observable filteredUsers = [];
+
+  start() {
+    this.users = User.find({}).then((data) => {
+      this.me = data.find(user => user.id === this.stores.Login.user.id);
+
+      let friends = data.filter(user => {
+        return user.id !== this.stores.Login.user.id;
+      });
+
+      const isIncluded = (friendId) => {
+        return this.stores.Login.user.contact.some(id => friendId.includes(id));
   }
+      this.filteredUsers = friends.filter(friend => !isIncluded(friend._id));
+    });
 }
