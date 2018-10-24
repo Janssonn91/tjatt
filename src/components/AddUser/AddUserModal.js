e44e27b56714de54da299b81f207cff344441336
Nana Mochihara <nana.mochihara@gmail.com>
2018-10-24 12:58:50 +0200

    Implement addContact function (in progress)

--- src/components/AddUser/AddUserModal.js
+++ src/components/AddUser/AddUserModal.js
@@ -2,6 +2,7 @@ import './AddUserModal.scss';
 
 @observer export default class AddUser extends Component {
 
+  @observable me = {};
   @observable users = [];
   @observable filteredUsers = [];
 
@@ -15,7 +16,36 @@ import './AddUserModal.scss';
 
       const isIncluded = (friendId) => {
         return this.stores.Login.user.contact.some(id => friendId.includes(id));
-  }
+      }
       this.filteredUsers = friends.filter(friend => !isIncluded(friend._id));
     });
+  }
+
+  addContact(userId) {
+    console.log(this.me);
+    this.me.contact.push(userId);
+    // const newContact =
+    console.log(toJS(this.me.contact))
+    this.me = { ...this.me, contact: toJS(this.me.contact) };
+    const currentUser = new User(this.me);
+    currentUser.save();
+
+    // friend.contact.push(user._id);
+    // const newfriend = new User(friend);
+    // newfriend.save();
+  }
 }
+
+        // this.stores.Login.user.contact.forEach(person => {
+        //   return friends.forEach(friend => {
+        //     if (friend._id === person._id) {
+        //       return;
+        //     } else {
+        //       this.stores.Login.user.contact.push(friend._id);
+        //       user = { ...user, contact: this.stores.Login.user.contact };
+        //       const currentUser = new User(user);
+        //       currentUser.save();
+        //     }
+        //   });
+        // })
+
