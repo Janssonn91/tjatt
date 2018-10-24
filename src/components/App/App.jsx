832a62dab2c6c4296d60838319e8436d0ea958af
PaulinaHT <paulina.hertor@gmail.com>
2018-10-24 10:55:49 +0200

    Merge conflict fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,10 @@
   {/* MobX observable style needs spread wrapping */}
   <div className="App d-flex flex-column" style={{...this.style}}>
     <Container fluid={true} tag="main" className="flex-grow-1">
-<<<<<<< HEAD
-      <Route exact path="/" component={Login} /> 
-      <Route path="/signup" component={Signup} />
-      <Route path="/server" component={Server} />
-
-        
-=======
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/server" component={Server} />
         {this.stores.Login && this.stores.Login.user && this.stores.Login.user.username ?
         <PrivateRoute path={`/${this.stores.Login.user.username}`} component={Tjatt} />
       : null}
@@ -21,7 +15,6 @@
           <p>This address could not be found</p>
         </Route>
       </Switch>
->>>>>>> chat-style-server
     </Container>
   </div>
 
