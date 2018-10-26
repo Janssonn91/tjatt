53b89f6475f75ab29ae68037f212c5e94ea4140f
Johan Anderberg <johan_anderberg79@hotmail.com>
2018-10-26 11:09:15 +0200

    Responsiveness issue

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -37,7 +37,7 @@
         <Col className="msg-area mt-5" sm={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }} lg={{ size: 4, offset: 4 }}>
           <h4 className="left-msg">Join for free and experience the power of tj@!</h4>
         </Col>
-        <Col className="msg-area mt-5" sm={{ size: 10, offset: 1 }} lg={{ size: 4, offset: 4 }}>
+        <Col className="msg-area mt-5" sm={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }} lg={{ size: 4, offset: 4 }}>
           <h4 className="right-msg">Build powerful node.js-apps and share with teams!</h4>
         </Col>
       </Row>
