4b077f9c8134c6894d1626012deba126ca6035dd
Nana Mochihara <nana.mochihara@gmail.com>
2018-10-27 21:41:46 +0200

    Move signUp function to login-store

--- src/store/login-store.js
+++ src/store/login-store.js
@@ -33,6 +33,29 @@ class LoginStore {
         console.log("err", err)
       })
   }
+
+  @action signUp(username, password) {
+    fetch('/api/users',
+      {
+        credentials: 'include',
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' }
+      }).then(res => res.json())
+      .then(res => {
+        if (res.success) {
+          console.log('created user: ' + username)
+
+          this.user = res.user
+          this.usernameExits = false;
+        } else {
+          this.usernameExits = true;
+        }
+      }).catch((err) => {
+        console.log('error', err);
+      });
+  }
+
 }
 
 export const loginStore = new LoginStore();
