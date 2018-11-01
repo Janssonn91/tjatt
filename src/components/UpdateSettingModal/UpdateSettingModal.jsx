f9ccc0fe5fca6aa32345f21d26402be3d2c88bff
Nana Mochihara <nana.mochihara@gmail.com>
2018-11-01 11:38:17 +0100

    Change layout of UpdateSettingModal

--- src/components/UpdateSettingModal/UpdateSettingModal.jsx
+++ src/components/UpdateSettingModal/UpdateSettingModal.jsx
@@ -1,45 +1,89 @@
 <Modal isOpen={this.props.isOpen} toggle={this.props.toggle} keyboard={this.props.keyboard} className="setting-modal">
   <ModalHeader tag="h4" toggle={this.props.toggle}>Profile settings</ModalHeader>
   <ModalBody>
-    <Form>
-      <FormGroup className="m-0">
-        <Label className="btn btn-upload m-0 p-0" htmlFor="files">Choose image</Label>
-        <Input className="d-none" id="files" type="file" name="files" onChange={this.onFileChange} />
-        <Label for="changeImage" tag="h5">Image</Label>
-        <p>Current Image: {this.props.loginStore.user.image}</p>
-        <Input type="file" name="image" id="changeImage" placeholder="image" />
-      </FormGroup>
-      <hr />
-      <FormGroup className="form-inline">
-        <Label for="changeNickname" tag="h5" className="mr-3">Nickname</Label>
-        <Input
-          type="text"
-          name="nickname"
-          id="changeNickname"
-          placeholder={this.props.loginStore.user.nickname}
-          value={this.nickname}
-          onChange={(e) => this.nickname = e.currentTarget.value}
-          style={{ width: "70%" }}
-        />
-      </FormGroup>
-      <hr />
-      <FormGroup>
-        <Label for="changePassword" tag="h5">Password</Label>
-        <p>You can change your password</p>
-        <Input
-          type="password"
-          name="password"
-          id="changePassword"
-          placeholder={this.props.loginStore.user.password}
-          value={this.currentPassword}
-          onChange={(e) => this.currentPassword = e.currentTarget.value}
-        />
-      </FormGroup>
-      <Button>Confirm</Button>
-    </Form>
+    <Table hover borderless>
+      <tbody>
+        <tr>
+          <th>Image upload</th>
+          <td>
+            <Label className="sr-only" for="changeImage" tag="h5">Image</Label>
+            <Input
+              type="file"
+              name="image"
+              id="changeImage"
+              placeholder="image"
+              style={{ borderColor: "black" }}
+              onChange={e => this.onFileChange(e)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>Nickname</th>
+          <td>
+            <Label className="sr-only" for="changeNickname" tag="h5">Nickname</Label>
+            <Input
+              type="text"
+              name="nickname"
+              id="changeNickname"
+              placeholder={this.props.loginStore.user.nickname}
+              value={this.nickname}
+              onChange={(e) => this.nickname = e.currentTarget.value}
+              style={{ width: "70%" }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <th colSpan="2">Password</th>
+        </tr>
+        <tr>
+          <td>current password</td>
+          <td>
+            <Label className="sr-only" for="changePassword" tag="h5">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="changePassword"
+              value={this.currentPassword}
+              onChange={e => this.currentPassword = e.currentTarget.value}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>new password</td>
+          <td>
+            <Label className="sr-only" for="newPassword1" tag="h5">New password</Label>
+            <Input
+              type="password"
+              name="newPassword1"
+              id="newPassword1"
+              value={this.newPassword1}
+              onChange={e => this.newPassword1 = e.currentTarget.value}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>new password</td>
+          <td>
+            <Label className="sr-only" for="newPassword2" tag="h5">New password</Label>
+            <Input
+              type="password"
+              name="newPassword2"
+              id="newPassword2"
+              value={this.newPassword2}
+              onChange={e => this.checkPassword(e)}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+    {this.isNotCorrectPass && <Alert className="alert-color">
+      Incorrect current password</Alert>}
+    {this.isNotSamePass && <Alert className="alert-color">
+      You filled in different password</Alert>}
   </ModalBody>
   <ModalFooter>
     <Button
+      disabled={this.isNotCorrectPass || this.isNotSamePass}
       className="btn btn-save"
       onClick={() =>
         this.props.loginStore.updateSettings({ nickname: this.nickname, password: this.newPassword, imageFormData: this.image })
