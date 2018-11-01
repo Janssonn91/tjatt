38c58eaf2bfbbac1d266064b20fee150c9719b1b
Hui Su <yudasuhui@hotmail.com>
2018-11-01 13:20:58 +0100

    fix create group modal check
    group name must have  check,
    group member at least more than 2

--- src/components/CreateGroupModal/CreateGroupModal.js
+++ src/components/CreateGroupModal/CreateGroupModal.js
@@ -14,8 +14,16 @@
 
   @action checkBeforeSubmit(){
     //check if groupName is available
+
+
+  }
+
+  @action createGroup(){
+    //check Before Submit;
+
     if(!this.groupName){
       this.myAttr='show';
+      return;
     }else{
       this.myAttr= 'd-none';
     }
@@ -23,20 +31,13 @@
 
     if(this.props.loginStore.selectedGroupMember.length<2){
       this.showAttr = 'show';
+      return;
     }else{
       this.showAttr = 'd-none';
     }
-    if(this.groupName && this.props.loginStore.selectedGroupMember.length>=2){
-      this.check= true;
-    }
-
-  }
-
-  @action createGroup(){
-    this.checkBeforeSubmit();
-    if(this.check){
-      this.props.channelStore.addChannel(this.groupName);
-    }
+    
+    this.props.channelStore.addChannel(this.groupName);
+    
    
   }
 
