f1681d529fdfe4b6dc50f9764b98e05e70e62a3a
Niklas J <Niklas.Jansson91@hotmail.com>
2018-11-07 00:19:51 +0100

    Small changes of style: sidebar (padding mostly) and sidebar dropdown.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,42 +7,42 @@
           <DropdownToggle tag="div" caret>
             <h5 className="ml-1">{this.props.loginStore.user.nickname || this.props.loginStore.user.username}</h5>
           </DropdownToggle >
-          <DropdownMenu tag="div">
-            <DropdownItem className="px-3 py-1 btn-li" tag="li" onClick={() => this.openModalupdateSetting()}>Settings</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem className="px-3 py-1 btn-li" tag="li" onClick={e => this.logout()}>Logout</DropdownItem>
+          <DropdownMenu className="p-0" tag="div">
+            <DropdownItem className="px-3 py-2 btn-li" tag="li" onClick={() => this.openModalupdateSetting()}>Settings</DropdownItem>
+            <DropdownItem className="m-0" divider />
+            <DropdownItem className="px-3 py-2 btn-li" tag="li" onClick={e => this.logout()}>Logout</DropdownItem>
           </DropdownMenu>
         </Dropdown>
       </div>
     </div>
     <hr className="mt-0" />
-    <Nav vertical className="menu pl-1">
+    <Nav vertical className="menu">
       {/* <NavLink to="#" className="p-0"><i className="fas fa-star pr-3 pr-md-2"></i>
         <NavItem className="pl-1">Starred</NavItem>
       </NavLink> */}
 
       <div className="flexWrapper">
-        <NavLink to="#">
+        <NavLink className="py-1 px-3" to="#">
           <NavItem>My Contacts</NavItem>
         </NavLink>
         <i onClick={this.openModalAddNewUser.bind(this)} className="fas fa-plus float-left float-md-none pl-4 pr-1 pl-md-0"></i>
       </div>
       <div id="contactsRender"></div>
       {/* {this.props.loginStore.myContacts.map((user, i) =>
-        <div key={i} className="nav-link pl-5 pl-md-3 contacts" onClick={() => this.props.channelStore.changeChannel("contact", user._id)}>
+        <div key={i} className="nav-link pl-5 pl-md-1 contacts" onClick={() => this.props.channelStore.changeChannel("contact", user._id)}>
           <CardImg className="mr-3 d-inline-block" src={user.image || "/images/placeholder.png"} />
           <div className="d-inline-block">{user.nickname}</div>
         </div>
       )} */}
       <div className="flexWrapper">
-        <NavLink to="#">
+        <NavLink className="py-1 px-3" to="#">
           <NavItem>My Groups</NavItem>
         </NavLink>
         <i onClick={this.openModalCreateGroup.bind(this)} className="fas fa-plus float-left float-md-none pl-4 pr-1 pl-md-0"></i>
       </div>
       <div id="groupsRender"></div>
       {/* {this.props.channelStore.myChannels.map((channel, i) => 
-      <NavLink key={i} className="nav-link pl-5 pl-md-3 contacts">
+      <NavLink key={i} className="nav-link pl-5 pl-md-1 contacts">
 
        <div className="d-inline-block" >{channel}</div>
        </NavLink>
