c376ab326861c3969ad6907e940f095c66e54a58
Niklas J <Niklas.Jansson91@hotmail.com>
2018-11-07 00:22:27 +0100

    Sidebar-padding alignment correctly.

--- src/store/channel-store.js
+++ src/store/channel-store.js
@@ -60,12 +60,12 @@ class ChannelStore {
         contact = await this.getContactName(channel.members);
       }
       return (type === 'group'   ?
-        <div key={i} className="nav-link pl-5 pl-md-3 contacts" onClick={() => this.changeChannel(channel)}>
+        <div key={i} className="nav-link pl-5 pl-md-4 py-md-1 contacts" onClick={() => this.changeChannel(channel)}>
           <div className="d-inline-block" >{channel.channelname} </div>
         </div>
       :
-        <div key={i} className="nav-link pl-5 pl-md-3 contacts" onClick={() => this.changeChannel(channel)}>
-          <CardImg className="mr-3 d-inline-block" src={contact.contactImg || "/images/placeholder.png"} />
+        <div key={i} className="nav-link pl-5 pl-md-4 py-md-1 contacts" onClick={() => this.changeChannel(channel)}>
+          <CardImg className="mr-2 d-inline-block" src={contact.contactImg || "/images/placeholder.png"} />
           <div className="d-inline-block" >{contact.contactChannelname}</div>
        </div>
        );
