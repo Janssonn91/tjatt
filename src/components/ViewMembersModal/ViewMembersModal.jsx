fd4169c645429d4e779f13b851b42eeb84f8667c
Niklas J <Niklas.Jansson91@hotmail.com>
2018-11-12 22:02:53 +0100

    Less margin on profile-image.

--- src/components/ViewMembersModal/ViewMembersModal.jsx
+++ src/components/ViewMembersModal/ViewMembersModal.jsx
@@ -7,31 +7,31 @@
       <ListGroup className="border-0 rounded-0 member-list">
 
         <ListGroupItem className="m-0 py-2 px-1 border-left-0 border-right-0 rounded-0 admin-user">
-          <CardImg className="mr-3 d-inline-block img" src={"/images/placeholder.png"}/>
+          <CardImg className="mr-2 d-inline-block img" src={"/images/placeholder.png"}/>
           <p className="m-0 p-0 d-inline">Nalle Pu</p>
           <p className="m-0 ml-2 p-0 d-inline admin-text">(Admin)</p>
         </ListGroupItem>
       
         <ListGroupItem className="m-0 py-2 px-1 border-left-0 border-right-0 rounded-0">
-          <CardImg className="mr-3 d-inline-block img" src={"/images/placeholder.png"}/>
+          <CardImg className="mr-2 d-inline-block img" src={"/images/placeholder.png"}/>
           <p className="m-0 p-0 d-inline">Nasse</p>
           <Button className="btn btn-make-admin border-0 float-right">Make admin</Button>
         </ListGroupItem>
 
         <ListGroupItem className="m-0 py-2 px-1 border-left-0 border-right-0 rounded-0">
-          <CardImg className="mr-3 d-inline-block img" src={"/images/placeholder.png"}/>
+          <CardImg className="mr-2 d-inline-block img" src={"/images/placeholder.png"}/>
           <p className="m-0 p-0 d-inline">Tiger</p>
           <Button className="btn btn-make-admin border-0 float-right">Make admin</Button>
         </ListGroupItem>
 
         <ListGroupItem className="m-0 py-2 px-1 border-left-0 border-right-0 rounded-0">
-          <CardImg className="mr-3 d-inline-block img" src={"/images/placeholder.png"}/>
+          <CardImg className="mr-2 d-inline-block img" src={"/images/placeholder.png"}/>
           <p className="m-0 p-0 d-inline">Åsnan</p>
           <Button className="btn btn-make-admin border-0 float-right">Make admin</Button>
         </ListGroupItem>
 
         <ListGroupItem className="m-0 py-2 px-1 border-left-0 border-right-0 rounded-0">
-          <CardImg className="mr-3 d-inline-block img" src={"/images/placeholder.png"}/>
+          <CardImg className="mr-2 d-inline-block img" src={"/images/placeholder.png"}/>
           <p className="m-0 p-0 d-inline">Kristoffer Robin</p>
           <Button className="btn btn-make-admin border-0 float-right">Make admin</Button>
         </ListGroupItem>
