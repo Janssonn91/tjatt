a2fd49bce6181a84ac3ad4a565c1c56b10724911
André Tente <iamandretente@gmail.com>
2018-11-14 17:46:38 +0100

    ready to trigger api route/ docker magic

--- src/components/GitApps/GitApps.js
+++ src/components/GitApps/GitApps.js
@@ -14,110 +14,126 @@ import './GitApps.scss';
 @observable loadingStatus = 0;
 @observable runningAppDropdown = false ;
 
-  async start(){
-    this.createStoreConnectedProperties({
-      repo: []
-    });
-    this.importingApps = true;
-    await this.fetchRepos();
-  }
-
-  async importRepo(name, url){
-    await sleep(1000);
-    await Repo.create({
-      name: name,
-      url: url,
-      port: "port",
-      running: false
-    })
-    .then(response=>{
-      this.importingRepo = false;
-      this.fetchRepos();
-      // this.importedApps.push(response);
-    })
-    .catch(
-      error=>console.log(error)
-    );
-  }
-
-  async deleteRepo(appId){
-    await this.importedApps.find(app=>app._id === appId).delete()
-    .then(response=>{
-      console.log('removed', response)
-    })
-    .catch(
-      error=>console.log(error)
-    );
-    this.fetchRepos();
-  }
-
-  async fetchRepos(){
-    this.importedApps = await Repo.find();
-    this.importingApps = false;
-  }
-
-  onUrlChangeHandler(e){
-    this.urlToSet = e.currentTarget.value;
-  }
-
-  checkForEnter(e){
-    if(e.key ==='Enter'){
-      this.submit()
+    async start(){
+        this.createStoreConnectedProperties({
+            repo: []
+        });
+        this.importingApps = true;
+        await this.fetchRepos();
     }
-  }
 
-  sideDrawerHandler(){
-    this.showSidedrawer = !this.showSidedrawer;
-  }
-
-  sideDrawerShowImportHandler(){
-    this.showImport = !this.showImport;
-  }
-
-  sideDrawerShowAppsHandler(){
-      this.showApps = !this.showApps;
-  }
-
-  onSubmitEnterHandler(event) {
-    if(event.key === 'Enter'  ){
-      event.preventDefault();
-      this.onSubmit();
+    async importRepo(name, url){
+        // await sleep(1000);
+        await Repo.create({
+            name: name,
+            url: url,
+            port: "port",
+            running: false
+        })
+        .then(response=>{
+            this.importingRepo = false;
+            this.fetchRepos();
+            // this.importedApps.push(response);
+        })
+        .catch(
+            error=>console.log(error)
+        );
     }
-  }
 
-  onSubmitClickHandler(){
-    this.onSubmit();
-  }
+    async deleteRepo(appId){
+        await this.importedApps.find(app=>app._id === appId).delete()
+        .then(response=>{
+            console.log('removed', response)
+        })
+        .catch(
+            error=>console.log(error)
+        );
+        this.fetchRepos();
+    }
 
-  runningAppDropdownHandler(){
-    this.runningAppDropdown = !this.runningAppDropdown;
-  }
+    async fetchRepos(){
+        this.importedApps = await Repo.find();
+        this.importingApps = false;
+    }
 
-  openAppHandler(app){
-    this.openApp._id === app._id ? this.openApp = {} : this.openApp = app;
-  }
-  closeAppHandler(){
-    this.openApp = {}
-  }
+    onUrlChangeHandler(e){
+        this.urlToSet = e.currentTarget.value;
+    }
 
-  onSubmit(){
-    this.importingRepo = true;
-    this.importRepo(this.urlToSet,this.urlToSet);
-    this.urlToSet = '';
-    this.projectToSet = '';
-  }
+    checkForEnter(e){
+        if(e.key ==='Enter'){
+            this.submit()
+        }
+    }
 
-  async runningAppHandler(appId){
-    const app = this.importedApps.find(app=> app._id === appId);
-    //this changes the running status
-    app.running = !app.running;
-    //this saves the new running status on the database so when we initially land on the page know which apps are running
-    await app.save();
-    this.fetchRepos();
-    //this creates an array of only running apps
-    this.runningApps = this.importedApps.filter(app=>app.running===true);
-    //this closes the open app if the running process is stoped
-    this.openApp._id === appId ? this.openApp = {} : null;
-  }
+    sideDrawerHandler(){
+        this.showSidedrawer = !this.showSidedrawer;
+    }
+
+    sideDrawerShowImportHandler(){
+        this.showImport = !this.showImport;
+    }
+
+    sideDrawerShowAppsHandler(){
+        this.showApps = !this.showApps;
+    }
+
+    onSubmitEnterHandler(event) {
+        if(event.key === 'Enter'  ){
+            event.preventDefault();
+            this.onSubmit();
+        }
+    }
+
+    onSubmitClickHandler(){
+        this.onSubmit();
+    }
+
+    runningAppDropdownHandler(){
+        this.runningAppDropdown = !this.runningAppDropdown;
+    }
+
+    openAppHandler(app){
+        this.openApp._id === app._id ? this.openApp = {} : this.openApp = app;
+    }
+    closeAppHandler(){
+        this.openApp = {}
+    }
+
+    async runningAppHandler(appId){
+        const app = this.importedApps.find(app=> app._id === appId);
+        //this changes the running status
+        app.running = !app.running;
+        //this saves the new running status on the database so when we initially land on the page know which apps are running
+        await app.save();
+        this.fetchRepos();
+        //this creates an array of only running apps
+        this.runningApps = this.importedApps.filter(app=>app.running===true);
+        //this closes the open app if the running process is stoped
+        this.openApp._id === appId ? this.openApp = {} : null;
+    }
+
+    async onSubmit(){
+        this.importingRepo = true;
+
+        //this should be removed when the fetch method is uncommented
+        this.importRepo(this.urlToSet,this.urlToSet);
+        this.urlToSet = '';
+        this.projectToSet = '';
+
+        
+        // await fetch('/api/addRepo', { 
+        //   headers:{'Content-Type': 'application/json'},
+        //   body: JSON.stringify({url: this.urlToSet, projectName: this.projectToSet}), // data can be `string` or {object}!
+        //   method: 'POST' // or 'PUT'
+        // })
+        // .then(response => {
+        //     this.importRepo(this.urlToSet,this.urlToSet);
+        //     this.urlToSet = '';
+        //     this.projectToSet = '';
+        // })
+        // .catch(error=>console.log(error));
+        
+    }
   
 }
