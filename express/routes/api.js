bc98b0dedee8b6b8efc1139ef530323739da03a2
Tin Kotvas <tin.kotvas@gmail.com>
2018-11-16 10:31:58 +0100

    bugfix and cleanup

--- express/routes/api.js
+++ express/routes/api.js
@@ -23,32 +23,33 @@ router.post('/addRepo', async (req, res) => {
     dockerPort: dockerPort,
     webPort: 3300,
     dbPort: 7000,
-    localPath: path.join(__dirname, "../../docker/" + uniqueProjectName)
+    localPath: path.join(__dirname, "../../docker/" + uniqueProjectName),
+    res: res
   }
   fs.existsSync(payload.localPath) ?
-    del(payload.localPath).then(() => git_clone(payload, res)) : git_clone(payload, res);
+    del(payload.localPath).then(() => git_clone(payload)) : git_clone(payload);
 
 });
 
-function git_clone(payload, res) {
+function git_clone(payload) {
   simplegit()
     .silent(true)
     .clone(payload.gitUrl, payload.localPath)
     .then(err => {
       console.log("Downloaded repo from: " + payload.gitUrl);
       console.log("Proceeding with building Docker image")
-      prepare_docker_files(payload, res);
+      prepare_docker_files(payload);
     })
-    .catch(err => { console.log("error", err); res.json('err'); });
+    .catch(err => { console.log("error", err); payload.res.json('err'); });
 }
 
 
-async function prepare_docker_files(payload,res) {
-  let did = await create_docker_dockerfile(payload, res);
-  did && create_docker_compose_file(payload,res)
+async function prepare_docker_files(payload) {
+  let did = await create_docker_dockerfile(payload);
+  did && create_docker_compose_file(payload)
 }
 
-async function get_used_ports(res) {
+async function get_used_ports() {
   return new Promise((resolve, reject) => {
     docker.container.list().then(containers => {
       let ports = containers.filter(container => {
@@ -68,17 +69,17 @@ async function get_used_ports(res) {
   })
 }
 
-async function select_docker_port(res) {
+async function select_docker_port() {
   let probePort = 49152;
   let found = false;
-  let usedPorts = await get_used_ports(res);
+  let usedPorts = await get_used_ports();
   while (!found) {
     usedPorts.includes(probePort) ? probePort++ : (found = true);
   }
   return probePort;
 }
 
-function create_docker_dockerfile(payload, res) {
+function create_docker_dockerfile(payload) {
   let path = `./docker/${payload.uniqueProjectName}/Dockerfile`;
   return new Promise((promiseResult) =>{
     fs.writeFile(path, '', {
@@ -86,7 +87,6 @@ function create_docker_dockerfile(payload, res) {
     }, function (err) {
       if (err) {
         console.log("Dockerfile already exists!")
-        res.json('error ...');
         promiseResult(false);
       } else {
         fs.appendFileSync(path,
@@ -104,7 +104,7 @@ function create_docker_dockerfile(payload, res) {
 
 }
 
-function create_docker_compose_file(payload, res) {
+function create_docker_compose_file(payload) {
   let path = `./docker/${payload.uniqueProjectName}/docker-compose.yml`;
 
   console.log('payload', payload)
@@ -130,10 +130,9 @@ services:
   }, function (err) {
     if (err) {
       console.log("Docker compose file already exists!");
-      res.json('error');
     } else {
       fs.appendFileSync(path, data);
-      start_containers_composer(payload, res);
+      start_containers_composer(payload);
     }
   });
 }
@@ -142,15 +141,15 @@ const {
   exec
 } = require('child_process');
 
-function start_containers_composer(payload, res) {
+function start_containers_composer(payload) {
   exec(`docker-compose up -d`, {
     cwd: payload.localPath
   }, (err, stdout, stderr) => {
     if (err) {
-      res.json('error');
       throw (err);
     }
-    res.json(payload);
+    let response = Object.assign({}, payload, {res: null})
+    payload.res.json(response);
     console.log(stdout || stderr);
   });
 }
