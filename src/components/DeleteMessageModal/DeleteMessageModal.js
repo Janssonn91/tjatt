53bcd4a17995eee3a50c62e430ca37edcea5cc7d
Nana Mochihara <nana.mochihara@gmail.com>
2018-11-16 11:15:06 +0100

    Implement deleteMessage function(in progress)

--- src/components/DeleteMessageModal/DeleteMessageModal.js
+++ src/components/DeleteMessageModal/DeleteMessageModal.js
@@ -1,5 +1,21 @@
 import './DeleteMessageModal.scss'
 
 export default class DeleteMessageModal extends Component {
-  async start() {}
+
+  deleteMessage() {
+    fetch(`/api/message/${this.props.messageId}/delete`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(result => {
+        console.log('result', result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
 }
