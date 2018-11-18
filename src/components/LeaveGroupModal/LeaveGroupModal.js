852c3219d4dea1ab3846346516e1063818603be8
Johan Anderberg <johan_anderberg79@hotmail.com>
2018-11-18 20:57:19 +0100

    Refactoring from channelstore to components

--- src/components/LeaveGroupModal/LeaveGroupModal.js
+++ src/components/LeaveGroupModal/LeaveGroupModal.js
@@ -1,9 +1,186 @@
 import './LeaveGroupModal.scss';
 
-@inject('channelStore') export default class LeaveGroupModal extends Component {
+@inject('loginStore', 'channelStore') export default class LeaveGroupModal extends Component {
+
   leaveChannel(){
-    let channelToLeave = this.props.channelStore.currentChannel;
-    this.props.channelStore.exitChannel(channelToLeave);
+    let channel = this.props.channelStore.currentChannel;
+
+    for (let channelArr of this.props.channelStore.myChannels) {
+      if (channelArr._id === channel._id) {
+        const index = channel.members.indexOf(this.props.loginStore.user._id);
+        if (index > 0) {
+          channel.members.splice(index, 1);
+        };
+      }
+    }
+  
+    // remove the channel from the user and re-render users channels
+    let i = 0;
+    for (let channelArr of this.props.channelStore.groupChannels) {
+      if (channelArr._id === channel._id) {
+        this.props.channelStore.spliceChannel(i);
+      }
+      i++;
+    }
+    this.props.channelStore.renderChannels();
+
+    // remove both channel from user and user from channel in backend
+    const userId = this.props.loginStore.user._id;
+    fetch(`/api/memberChannels/${channel._id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        userid: userId
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        return res.json();
+      }).then(res => {
+        console.log(res.resultChannel, res.resultUser)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    if (this.props.channelStore.amIAdmin) {
+      this.removeAdmin(userId, channel);
+    }
+    if (this.props.channelStore.currentGroupMembers.length === 1) {
+      this.deleteGroup(channel);
+    }
+    window.history.pushState(null, null, '/' + this.props.loginStore.user.username);
     this.props.toggle();
   }
+
+  deleteGroup(channel) {
+    fetch(`/api/removeGroup/${channel._id}`, {
+      method: 'DELETE'
+    })
+      .then(res => {
+        res.json()
+      })
+      .then(() => {
+        console.log(`slut på group, ${channel._id}`)
+      })
+  }
+
+  removeAdmin(id, channel) {
+    // remove admin from frontend
+    let index = this.props.channelStore.currentChannelAdmins.indexOf(id);
+    this.props.channelStore.spliceAdmin(index);
+    // remove admin from db
+    fetch(`/api/removeAdmin/${channel._id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        userid: id
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        return res.json();
+      }).then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    this.props.channelStore.adminStatus()
+  }
+
 }
+
+
+// moved to leavegroupmodal, saved for reference only
+// @action exitChannel(channel) {
+//   // remove the user from the channel
+//   // for (let channelArr of this.myChannels) {
+//   //   if (channelArr._id === channel._id) {
+//   //     const index = channel.members.indexOf(loginStore.user._id);
+//   //     if (index > 0) {
+//   //       channel.members.splice(index, 1);
+//   //     };
+//   //   }
+//   // }
+
+//   // // remove the channel from the user and re-render users channels
+//   // let i = 0;
+//   // for (let channelArr of this.groupChannels) {
+//   //   if (channelArr._id === channel._id) {
+//   //     this.groupChannels.splice(i, 1);
+//   //   }
+//   //   i++;
+//   // }
+//   // this.renderChannels();
+
+//   // test att lämna grupp och historik i frontend
+//   this.ChannelChatHistory = [];
+//   this.currentChannelGroup = false;
+//   this.channelName = '';
+
+//   // remove both channel from user and user from channel in backend
+//   const userId = loginStore.user._id;
+//   fetch(`/api/memberChannels/${channel._id}`, {
+//     method: 'PUT',
+//     body: JSON.stringify({
+//       userid: userId
+//     }),
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+//   })
+//     .then(res => {
+//       return res.json();
+//     }).then(res => {
+//       console.log(res.resultChannel, res.resultUser)
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+//   if (this.amIAdmin) {
+//     this.removeAdmin(userId, channel);
+//   }
+//   if (this.currentGroupMembers.length === 1) {
+//     this.deleteGroup(channel);
+//   }
+//   window.history.pushState(null, null, '/' + loginStore.user.username);
+// }
+
+// deleteGroup(channel) {
+//   fetch(`/api/removeGroup/${channel._id}`, {
+//     method: 'DELETE'
+//   })
+//     .then(res => {
+//       res.json()
+//     })
+//     .then(() => {
+//       console.log(`slut på group, ${channel._id}`)
+//     })
+// }
+
+// removeAdmin(id, channel) {
+//   // remove admin from frontend
+//   let index = this.currentChannel.admin.indexOf(id);
+//   this.currentChannel.admin.splice(index, 1);
+//   // remove admin from db
+//   fetch(`/api/removeAdmin/${channel._id}`, {
+//     method: 'PUT',
+//     body: JSON.stringify({
+//       userid: id
+//     }),
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+//   })
+//     .then(res => {
+//       return res.json();
+//     }).then(res => {
+//       console.log(res)
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+//   this.amIAdmin = false;
+// }
--- src/components/ViewMembersModal/ViewMembersModal.js
+++ src/components/ViewMembersModal/ViewMembersModal.js
@@ -4,8 +4,48 @@ import './ViewMembersModal.scss';
 @observer export default class ViewMembersModal extends Component {
   
   setNewAdmin(e, userId){
+    // set new admin on frontend
     this.props.channelStore.setAdmin(userId);
-    this.forceUpdate();
+    // set new admin on backend
+    fetch(`/api/updateAdmin/${this.props.channelStore.currentChannel._id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        adminId: userId
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        return res.json();
+      }).then(res => {
+        console.log('admin updated: ', res)
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
 }
+
+// moved from setAdmin in channelstore, saved for reference only
+    // this.currentChannel.admin = [...this.currentChannel.admin, newAdminId];
+    // console.log(this.currentChannel);
+    // console.log(this.currentChannel._id);
+    // fetch(`/api/updateAdmin/${this.currentChannel._id}`, {
+    //   method: 'PUT',
+    //   body: JSON.stringify({
+    //     adminId: newAdminId
+    //   }),
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    //   .then(res => {
+    //     return res.json();
+    //   }).then(res => {
+    //     console.log('admin updated: ', res)
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   })
