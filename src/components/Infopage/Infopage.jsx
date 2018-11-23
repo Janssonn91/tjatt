f8d7e5f81763a1fab54c1283fefbf106615c2ab6
Niklas J <Niklas.Jansson91@hotmail.com>
2018-11-23 14:36:46 +0100

    Screenshot to use on infopage

---
+++ src/components/Infopage/Infopage.jsx
@@ -0,0 +1,28 @@
+<Fragment>
+  <Row className="infopage">
+    <Col className="col-12">
+      <h2 className="text-center mb-4">Introduction</h2>
+    </Col>
+    <Col className="col-12 main-info">
+      <Row className="settings-info">
+        <Col className="col-6">
+          <img src={"/images/settings-dropdown-menu.PNG"} className="img-fluid d-block mb-1"/>
+          <img src={"/images/settings.PNG"} className="img-fluid d-block mt-1"/>
+        </Col>
+        <Col className="col-6">
+          <p>test text</p>
+        </Col>
+      </Row>
+      <hr/>
+      <Row className="sidebar-info">
+        <Col className="col-6">
+          <img src={"/images/create-group.PNG"} className="img-fluid d-block mb-1"/>
+          <img src={"/images/search-contacts.PNG"} className="img-fluid d-block mt-1"/>
+        </Col>
+        <Col className="col-6">
+          <p>test text</p>
+        </Col>
+      </Row>
+    </Col>
+  </Row>
+</Fragment>
