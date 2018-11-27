a9c959620a77350b517e85cf03b1016d9f6456b9
Emil <emil_emil94@hotmail.com>
2018-11-27 12:33:02 +0100

    fluid img

--- src/components/ChatImgModal/ChatImgModal.jsx
+++ src/components/ChatImgModal/ChatImgModal.jsx
@@ -2,7 +2,7 @@
   <Modal isOpen={this.props.toggleVal} toggle={this.props.toggleModal}>
     <ModalHeader toggle={this.props.toggleModal} className="modal-header">{this.props.title}</ModalHeader>
     <ModalBody className="image-modal-body">
-      <img src={this.props.imgPath} alt="chat-img" />
+      <img src={this.props.imgPath} alt="chat-img" className="img-fluid" />
     </ModalBody>
     <ModalFooter>
       <Button color="secondary" onClick={this.props.toggleModal}>Close</Button>
