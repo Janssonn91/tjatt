bedeb08cfd68d48fc21ca0c0be23100946886bc2
Nana Mochihara <nana.mochihara@gmail.com>
2018-11-30 12:16:50 +0100

    Fix URL handling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -70,7 +70,7 @@
               <Link
                 to={`/${this.props.userStore.user.username}/${channel.channelname}`}
                 key={i}
-                onClick={() => this.props.channelStore.changeChannel(channel)}
+                onClick={() => setTimeout(this.loadChannelFromUrl.bind(this), 10)}
               >
                 <div className="nav-link pl-3 py-1 pr-1 contacts">
                   <CardImg className="mr-2 d-inline-block" src={channel.image || "/images/placeholder.png"} />
@@ -105,7 +105,7 @@
                 <Link
                   to={`/${this.props.userStore.user.username}/${channel.channelname}`}
                   key={i}
-                  onClick={() => this.props.channelStore.changeChannel(channel)}
+                  onClick={() => setTimeout(this.loadChannelFromUrl.bind(this), 10)}
                 >
                   <div className="nav-link pl-3 py-1 pr-1 contacts">
                     <span className="d-inline-block">{channel.channelname}</span>
--- src/store/channel-store.js
+++ src/store/channel-store.js
@@ -14,6 +14,7 @@ class ChannelStore {
   //@observable currentChannelGroup = false; // never used, removable ??
   @observable contactChannels = [];
   @observable groupChannels = [];
+  @observable hasLoadedChannels = false;
   @observable currentGroupMembers = [];
   @observable currentGroupCandidates = [];
   @observable hideMenu = true;
@@ -100,6 +101,7 @@ class ChannelStore {
     });
     await sleep(100);
     this.sortListByMessageNum();
+    this.hasLoadedChannels = true;
   }
 
   @action getGroupMembersData(memberIds) {
