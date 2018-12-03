8e982f4e4ea9dcbec2d9de9d07a309267a3dc9c0
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-03 14:47:51 +0100

    Remove chackbox in AddDeleteModal
    because it shows all candidates at first

--- src/components/AddDeleteMemberModal/AddDeleteMemberModal.jsx
+++ src/components/AddDeleteMemberModal/AddDeleteMemberModal.jsx
@@ -41,8 +41,6 @@
         <Row className="select-area">
           <Col sm="12" md="6" className="pl-0 pr-1 searched-user ">
             <h5>Searched user</h5>
-            <Label for="viewAllUsers" className="group-modal-text">View all users</Label>
-            <Input id="viewAllUsers" className="ml-2" type="checkbox" onChange={this.checkboxHandler}></Input>
             <FormGroup className="m-0 overflow-y-auto">
               {this.searchedGroupCandidates.map((user, i) =>
                 <ListGroupItem key={i} className="nav-link p-0 pl-1 contacts">
