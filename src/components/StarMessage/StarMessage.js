fa4ba5a383cbf78ee8fe293afa490c77832942e7
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-13 12:12:44 +0100

    Merge

---
+++ src/components/StarMessage/StarMessage.js
@@ -0,0 +1,67 @@
+@inject('userStore', 'channelStore') export default class StarMessage extends Component {
+
+  componentDidUpdate() {
+    console.log(this.props);
+  }
+
+  toggleSnippetHeight = (index) => {
+    if (this.fullHeightSnippet.some(obj => obj.index === index)) {
+      let scroll = document.querySelector('._scrollable-div_1dj6m_1');
+      let objIndex = this.fullHeightSnippet.findIndex(obj => obj.index === index);
+      scroll.scrollTop = this.fullHeightSnippet[objIndex].scroll;
+      this.fullHeightSnippet = this.fullHeightSnippet.filter(x => x.index !== index);
+
+    } else {
+      let scroll = document.querySelector('._scrollable-div_1dj6m_1').scrollTop;
+      console.log(scroll, { index, scroll })
+      this.fullHeightSnippet.push({ index, scroll });
+    }
+    console.log(this.fullHeightSnippet);
+  }
+
+  formattedTime(t) {
+    t = new Date(t);
+    let result = "";
+    let today = new Date();
+    let hour = t.getHours();
+    let min = t.getMinutes();
+    if (min < 10) {
+      min = "0" + min;
+    }
+    let ampm = hour <= 12 ? "AM" : "PM";
+    hour = hour % 12;
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    hour = hour ? hour : 12;
+    if (today.setHours(0, 0, 0, 0) === t.setHours(0, 0, 0, 0)) {
+      result += hour + ":" + min + " " + ampm + " Today";
+      return result;
+    } else {
+      result += t.getFullYear() + "/" + (t.getMonth() + 1) + "/" + t.getDate()
+        + " " + hour + ":" + min + " " + ampm;
+      return result;
+    }
+  }
+
+  updateStar(message, currentStar) {
+    const star = !currentStar;
+
+    // update star of message in backend
+    fetch(`/api/message/${message._id}/star`, {
+      credentials: 'include',
+      method: 'PUT',
+      body: JSON.stringify({ star }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(result => {
+        if (result.success) {
+          console.log('updated star');
+          // update myStars array in frontend
+          this.props.userStore.updateMyStars(message, star);
+        }
+      })
+      .catch(err => console.log(err));
+  }
+}
---
+++ src/components/StarMessage/StarMessage.jsx
@@ -0,0 +1,26 @@
+<li key={this.props.index} className="clearfix">
+  <div className="message-data">
+    <span>
+      <img alt="user-img" src={this.props.channelStore.userDict[this.props.message.sender].img || "/images/placeholder.png"} />
+    </span>&nbsp; &nbsp;
+    <span className="message-data-name">{this.props.channelStore.userDict[this.props.message.sender].name}</span>
+    <span className="message-data-time">{this.formattedTime(this.props.message.time)}</span>
+  </div>
+  <div className={`message other-message ${this.props.message.contentType === 'image' ? 'removeBG' : ''} ${this.props.message.contentType === 'code' ? 'code-snippet' : ''}`}>
+    {this.props.message.contentType === 'text' && this.props.message.text}
+    {this.props.message.contentType === 'file' && <a className="text-light files" href={this.props.message.filePath} download={this.props.message.originalName}>{this.props.message.originalName}<i className="far fa-file-alt pl-2"></i></a>}
+    {this.props.message.contentType === 'image' && <div className="img-upload-holder"><img src={this.props.message.filePath} className="upload-image" alt="chat-img" onClick={() => { this.toggleChatModal(); this.currentImage = this.props.message.filePath; this.originalName = this.props.message.originalName }} /></div>}
+    {this.props.message.contentType === 'code' && <div data-index={i} className={`${!this.fullHeightSnippet.some(obj => obj.index === i) ? 'highlight-small' : 'highlight-big'}`}><SyntaxHighlighter style={vs2015} showLineNumbers={true}>{this.props.message.text}</SyntaxHighlighter>
+      <Button onClick={() => this.toggleSnippetHeight(i)} color="secondary" className="full-height-btn"><i className={`fas fa-arrow-${this.fullHeightSnippet.some(obj => obj.index === i) ? 'up' : 'down'}`}></i></Button></div>}
+  </div>
+  <div className="float-right">
+    {this.props.userStore.myStars.some(s => s._id === this.props.message._id) ?
+      <i className="fas fa-star"
+        onClick={() => this.updateStar(this.props.message, true)}>
+      </i> :
+      <i className="far fa-star"
+        onClick={() => this.updateStar(this.props.message, false)}>
+      </i>
+    }
+  </div>
+</li>
