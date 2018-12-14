2d03787e5712cf8da4fd165b4523b8c10a2ac988
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-14 14:23:48 +0100

    Add other color to my message in star page

--- src/components/StarMessage/StarMessage.jsx
+++ src/components/StarMessage/StarMessage.jsx
@@ -11,7 +11,12 @@
     <span className="message-data-name">{this.props.channelStore.userDict[this.props.message.sender].name}</span>
     <span className="message-data-time">{this.formattedTime(this.props.message.time)}</span>
   </div>
-  <div className={`message other-message ${this.props.message.contentType === 'image' ? 'removeBG' : ''} ${this.props.message.contentType === 'code' ? 'code-snippet' : ''}`}>
+  <div
+    className={
+      `message ${this.props.channelStore.userDict[this.props.message.sender].name === this.props.userStore.user.username ? 'my-message' : 'other-message'}
+      ${this.props.message.contentType === 'image' ? 'removeBG' : ''}
+      ${this.props.message.contentType === 'code' ? 'code-snippet' : ''}`
+    }>
     {this.props.message.contentType === 'text' && this.props.message.text}
     {this.props.message.contentType === 'file' && <a className="text-light files" href={this.props.message.filePath} download={this.props.message.originalName}>{this.props.message.originalName}<i className="far fa-file-alt pl-2"></i></a>}
     {this.props.message.contentType === 'image' && <div className="img-upload-holder"><img src={this.props.message.filePath} className="upload-image" alt="chat-img" onClick={() => { this.toggleChatModal(); this.currentImage = this.props.message.filePath; this.originalName = this.props.message.originalName }} /></div>}
