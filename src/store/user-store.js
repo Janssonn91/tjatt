508bd0bc31ee490f3867e3efeb1e74434fe59ab5
Hui Su <yudasuhui@hotmail.com>
2018-12-20 12:33:52 +0100

    fix bug for groupcandidates not include pending user

--- src/store/user-store.js
+++ src/store/user-store.js
@@ -5,6 +5,7 @@ class UserStore {
   @observable user = { _id: "", channel: [], contact: [] };
   @observable isLoggedIn = false;
   @observable candidates = []; // AddUserModal
+  @observable notYetMyContact =[];
   @observable groupCandidates = []; //CreateGroupModal (groupCandidates mean myContacts)
   @observable selectedGroupMember = []; // CreateGroupModal, channel-store
   @observable isLoading = true;
@@ -30,7 +31,8 @@ class UserStore {
   }
 
   @action updateMyContact(userId) {
-    const addedUser = this.candidates.find(user => user._id === userId);
+    const addedUser = this.notYetMyContact.find(user => user._id === userId);
+    console.log("addeduser", addedUser);
     const index = this.candidates.indexOf(addedUser);
     this.candidates.splice(index, 1);
     this.groupCandidates.push(addedUser);
@@ -57,7 +59,7 @@ class UserStore {
       .then(res => res.json())
       .then(users => {
         let withoutMe = users.filter(user => user._id !== this.user._id);
-        withoutMe = withoutMe.filter(user => user._id !== applicationStateStore.systemId.toString());
+        this.notYetMyContact = withoutMe.filter(user => user._id !== applicationStateStore.systemId.toString());
 
         const isIncludedInContact = (userId) => {
           return this.user.contact.some(contactId => userId === contactId);
